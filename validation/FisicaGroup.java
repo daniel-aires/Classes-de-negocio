0d9e67e3d0af8917f05e0ce4b22d7adeeb74e792
Daniel Aires <31041494+daniel-aires@users.noreply.github.com>
2018-08-14 13:31:57 -0300

    Add files via upload

---
+++ validation/FisicaGroup.java
@@ -0,0 +1,8 @@
+package br.com.getjob.model.validation;
+/**
+*
+* @author DanielFilho
+*/
+public interface FisicaGroup {
+
+}
---
+++ validation/JuridicaGroup.java
@@ -0,0 +1,10 @@
+package br.com.getjob.model.validation;
+
+/**
+ *
+ * @author DanielFilho
+ */
+
+public interface JuridicaGroup {
+
+}
